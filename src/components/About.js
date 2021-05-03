d9f973471a033df0ae9024b2b3c0505bfc8f90d6
Tommy <thuang20018@yahoo.com>
2021-05-03 02:26:57 -0400

    added to about file

--- src/components/About.js
+++ src/components/About.js
@@ -1,7 +1,20 @@
 import React, { useEffect, useState } from "react";
+import sanityClient from "../client.js";
+import BlockContent from "@sanity/block-content-to-react"
 import lights from "../lights.jpg";
 
 export default function About() {
+  const [author, setAuthor] = useState(null);
+
+  useEffect(() => {
+    sanityClient.fetch(`*[_type == "author"]{
+      name,
+      bio,
+    }`).then((data) => setAuthor(data[0]))
+    .catch(console.error);
+  }, []);
+
+  if (!author) return <div>Loading...</div>
 
   return (
     <main className="relative">
@@ -9,11 +22,9 @@ export default function About() {
       <div className="p-5 lg:pt-40 container mx-auto relative">
         <section className="bg-green-500 rounded-lg shadow-2xl lg:flex p-20 ">
           <div className="text-lg flex flex-col justify-center">
-            <h1 className="cursive text-6xl text-purple-300 mb-4">About Me</h1>
+            <h1 className="cursive text-6xl text-purple-300 mb-4">About me</h1>
             <div className="lg:prose-xl text-white">
-              My name is Tommy Huang and I love technology, sports, games and anything that contains action. My
-              main focus is the field of Software Engineer. I am familiar
-              with cloud technologies such as Amazon Web Service. During free time I love to go outside to travel and explore and learn new things.
+              <BlockContent blocks={author.bio} projectId="5ny8ab4u" dataset="production"></BlockContent>
             </div>
           </div>
         </section>
