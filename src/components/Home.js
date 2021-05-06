524bdcdd3694138f6436cefc0cec746e1edcbd7e
Tommy <thuang20018@yahoo.com>
2021-05-05 20:57:56 -0400

    added typewriter effect

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,12 +1,19 @@
 import React from "react"
 import image from "../background.jpg";
+import Typerwriter from "typewriter-effect";
 
 export default function Home() {
         return (
             <main>
                 <img src={image} alt="Lobby" className="absolute object-cover w-full h-full" />
                 <section className="relative flex justify-center min-h-screen pt-12 lg:pt-64 px-8">
-                    <h1 className="text-6xl text-green-100 font-bold cursive leading-none lg:leading-snug home-name">Welcome, feel free to look around</h1>
+                    <h1 className="text-6xl text-green-100 font-bold cursive leading-none lg:leading-snug home-name">
+                    <Typerwriter 
+                        onInit={(typewriter) => {
+                            typewriter.typeString("Welcome").start();
+                        }}
+                        />
+                        </h1>
                 </section>
             </main>
         )
